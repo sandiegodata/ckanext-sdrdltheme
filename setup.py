aa40eeeac1ffa6bfa14928f76194da45fe813823
Eric Busboom <ckan@vagrd.cnshost.net>
2013-05-27 14:22:15 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-sdrdltheme',
+	version=version,
+	description="Design extension for SDRDL",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Eric Busboom',
+	author_email='eric@sandiegodata.org',
+	url='http://www.sandiegodata.org',
+	license='',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.sdrdltheme'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.sdrdltheme:PluginClass
+	""",
+)
